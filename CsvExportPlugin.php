770cf97f14ad00b1f89afb02d4530735b881f5eb
Dickson Law <dickson.law@utoronto.ca>
2017-02-16 17:18:57 -0500

    Added JavaDoc comments to the main plugin and the helper functions

--- CsvExportPlugin.php
+++ CsvExportPlugin.php
@@ -6,7 +6,11 @@ class CsvExportPlugin extends Omeka_Plugin_AbstractPlugin
         'action_contexts',
     );
     
-    // Sets the response MIME types
+    /**
+     * HOOK respond_contexts: Adds the response MIME types for the CSV export format
+     * @param array $contexts
+     * @return array
+     */
     public function filterResponseContexts($contexts) {
         $contexts['csv'] = array(
             'suffix' => 'csv',
@@ -18,11 +22,18 @@ class CsvExportPlugin extends Omeka_Plugin_AbstractPlugin
         return $contexts;
     }
     
-    // Add as an export on items browse/show and collections show
+    /**
+     * HOOK action_contexts: Add CSV as an export on Items browse/show and Collections show actions
+     * @param array $contexts
+     * @param array $args
+     * @return array
+     */
     public function filterActionContexts($contexts, $args) {
+        // Browse and show views for Items
         if ($args['controller'] instanceof ItemsController) {
             $contexts['browse'][] = 'csv';
             $contexts['show'][] = 'csv';
+        // Show view for Collections
         } elseif ($args['controller'] instanceof CollectionsController) {
             $contexts['show'][] = 'csv';
         }
