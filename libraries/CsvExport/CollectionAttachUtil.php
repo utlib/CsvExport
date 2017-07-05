938283e7a3ff3a20503cc6ebdc8aeb2fce301cf0
Dickson Law <dickson.law@utoronto.ca>
2017-07-05 15:36:40 -0400

    Remove nested collection support for now

--- libraries/CsvExport/CollectionAttachUtil.php
+++ libraries/CsvExport/CollectionAttachUtil.php
@@ -8,28 +8,14 @@ class CsvExport_CollectionAttachUtil {
     public static function getSubitems($collection) {
         // Set up holder for subitems
         $subItems = array();
-        // If this is a Collection-type collection
-        if (CsvExport_IiifItemsUtil::isCollection($collection)) {
-            // For each sub-collection
-            foreach (CsvExport_IiifItemsUtil::findSubmembersFor($collection) as $subCollection) {
-                // Recurse and join to subitem list
-                foreach (CsvExport_CollectionAttachUtil::getSubitems($subCollection) as $subItem) {
-                    $subItems[] = $subItem;
-                }
-            // End: For each sub-collection
+        // For each sub-item under the collection
+        $items = get_db()->getTable('Item')->findBy(array('collection' => $collection->id));
+        foreach ($items as $item) {
+            // Recurse and join to subitem list
+            foreach(CsvExport_ItemAttachUtil::getThisAndAnnotations($item) as $subItem) {
+                $subItems[] = $subItem;
             }
-        // Otherwise, if it this is a Manifest-type collection
-        } else {
-            // For each sub-item under the collection
-            $items = get_db()->getTable('Item')->findBy(array('collection' => $collection->id));
-            foreach ($items as $item) {
-                // Recurse and join to subitem list
-                foreach(CsvExport_ItemAttachUtil::getThisAndAnnotations($item) as $subItem) {
-                    $subItems[] = $subItem;
-                }
-            // End: For each sub-collection under the collection
-            }
-        // End: If this is a Collection-type collection
+        // End: For each sub-item under the collection
         }
         // Return all subitems
         return $subItems;
--- libraries/CsvExport/IiifItemsUtil.php
+++ libraries/CsvExport/IiifItemsUtil.php
@@ -18,45 +18,6 @@ class CsvExport_IiifItemsUtil {
     }
     
     /**
-     * Find subsidiary IIIF collections and manifests under this Omeka collection.
-     * @param Collection $collection
-     * @return Collection[]
-     */
-    public static function findSubmembersFor($collection) {
-        $myUuid = self::raw_iiif_metadata($collection, 'iiifitems_collection_uuid_element');
-        if (!$myUuid) {
-            return null;
-        }
-        $matches = get_db()->getTable('ElementText')->findBySql(
-            'element_texts.element_id = ? AND element_texts.text = ?',
-            array(get_option('iiifitems_collection_parent_element'), $myUuid)
-        );
-        $results = array();
-        foreach ($matches as $match) {
-            $candidate = get_record_by_id($match->record_type, $match->record_id);
-            $results[] = $candidate;
-        }
-        return $results;
-    }
-    
-    /**
-     * Return whether this collection is set to the Collection type.
-     * @param Collection $collection
-     * @return boolean
-     */
-    public static function isCollection($collection) {
-        try {
-            $iiifMetadataSlug = 'iiifitems_collection_type_element';
-            $iiifTypeText = self::raw_iiif_metadata($collection, $iiifMetadataSlug);
-            if ($iiifTypeText) {
-                return $iiifTypeText == 'Collection';
-            }
-        } catch (Exception $ex) {
-        }
-        return false;
-    }
-    
-    /**
      * Return an array of annotations for a non-annotation item, as Item records.
      * @param type $item
      * @return array
